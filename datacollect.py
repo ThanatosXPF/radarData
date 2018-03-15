b75646e3a548a6c636e2ec0bcec4ac31d1631711
Weather <645992705xpf@gmail.com>
2018-03-15 09:26:25 +0800

    :

--- datacollect.py
+++ datacollect.py
@@ -1,7 +1,7 @@
 import os
 import re
 import shutil
-filename = "~/radar/radar_reflect_data_2017/REFdata/"
+filename = "/home/thanatos/Documents/radar_reflect_data_2017/REFdata/"
 filelist = os.listdir(filename)
 for file in filelist:
     if re.match(r"cappi_ref_\d+_500_0.ref",file):
