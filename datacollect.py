692a50688df95f5e6f67a8a67c68331c6873e0af
ThanatosXPF <thanatosxie@gmail.com>
2018-03-19 10:17:40 +0800

    123

--- datacollect.py
+++ datacollect.py
@@ -8,6 +8,10 @@ def find500():
     for file in filelist:
         if re.match(r"cappi_ref_\d+_500_0.ref",file):
             shutil.copy(filename+"/"+file,"2017_500/")
+def find3500():
+    for file in filelist:
+        if re.match(r"cappi_ref_\d+_3500_0.ref",file):
+            shutil.copy(filename+"/"+file,"2017_3500/")
 def countMonth():
     list = []
     for file in filelist:
@@ -17,9 +21,12 @@ def countMonth():
     print(list.sort())
     print(len(list))
 def grabSomeData():
-    file_path = '2017_500'
+    file_path = '2017_3500'
     filelist = os.listdir(file_path)
     for file in filelist:
         if "20170501" in file:
             shutil.copy(os.path.join(file_path,file),"20170501_500")
+
+def radarAnalyze():
+    pass
 grabSomeData()
